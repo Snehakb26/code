ed8be1060087ed8c961b6279132ad9564f3e06e1
Snehakb26 <64859153+Snehakb26@users.noreply.github.com>
2020-05-10 13:00:06 +0530

    uploaded file on list ,dictionary and tuple.

---
+++ code3.py
@@ -0,0 +1,10 @@
+#assigning values to a list
+list=[11,22,33,44]
+#accessing elements from a tuple
+mytuple=('Riya','Priya','Niya')
+print(mytuple[1])
+#Deleting different dictionary elements
+mydict={"chess":"tough","carroms":"moderate","ludo":"easy"}
+print(mydict)
+del mydict["ludo"]
+print(mydict)
